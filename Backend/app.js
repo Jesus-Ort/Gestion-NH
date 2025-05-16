53f804966d7ec94db7faeb707b4660a4e0a38039
JesusOrt <xJesus_Nevermor3@hotmail.com>
2025-05-16 17:38:13 -0400

    Vite cambiado, env agg y gitignore

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,14 @@
 const express = require('express')
+require('dotenv').config()
 const cors = require('cors');
 const axios = require('axios');
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.use(cors());
 app.use(express.json())
 
-app.get('/api/saludo', (req, res) => {
+app.get('/', (req, res) => {
     res.json({ mensaje: '¡Hola desde el backend!' });
 });
 
