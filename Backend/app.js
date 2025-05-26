229cc31fa886b1f88ae5541649d6bc67e1886fad
JesusOrt <xJesus_Nevermor3@hotmail.com>
2025-05-26 14:29:39 -0400

    Montando el front en el backend

--- Backend/app.js
+++ Backend/app.js
@@ -2,15 +2,29 @@ const express = require('express')
 require('dotenv').config()
 const cors = require('cors');
 const axios = require('axios');
+const { resolve } = require('path');
 const app = express()
 const port = process.env.PORT
 
 app.use(cors());
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.json({ mensaje: '¡Hola desde el backend!' });
+// Middleware para headers personalizados en archivos .js
+app.use((req, res, next) => {
+    if (req.path.endsWith('.js')) {
+        res.setHeader('Content-Type', 'application/javascript');
+    }
+    next();
 });
+
+// Servir archivos estáticos
+app.use(express.static(resolve(__dirname, 'dist')));
+
+// Manejar todas las rutas para SPA
+app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(resolve(__dirname, 'dist', 'index.html'));
+});
+
 
 app.listen(port, () => {
     console.log(`Servidor corriendo`)
